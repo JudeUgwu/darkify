816303a92705f820c518e2381027878ac3456917
JudeUgwu <38662862+JudeUgwu@users.noreply.github.com>
2021-02-11 11:22:32 +0100

    updated new changes

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+define("APP_NAME","Toozo");
+
+// application path
+define("APP_PATH","http://localhost/darkify");
+define("IMAGE_PATH","http://localhost/darkify/assets/images/");
+define("CSS_PATH","http://localhost/darkify/assets/css/");
+define("JAVASCRIPT_PATH","http://localhost/darkify/assets/js/");
+define("DASHBOARD_PATH","http://localhost/darkify/users/");
+
+
---
+++ users/profile.php
@@ -0,0 +1,37 @@
+
+<?php require_once "../config.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <title>Dashboard</title>
+        <?php require_once "includes/css.php"; ?>
+
+    </head>
+    <body class="sb-nav-fixed">
+
+    <?php require_once "includes/top-bar.php"; ?>
+
+        <div id="layoutSidenav">
+        <?php require_once "includes/side-bar.php"; ?>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4">Your Profile</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+
+
+
+                    </div>
+                </main>
+                <?php require_once "includes/footer.php"; ?>
+
+            </div>
+        </div>
+        <?php require_once "includes/scripts.php"; ?>
+
+    </body>
+</html>
